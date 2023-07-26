729450ebb1de98a3ca8317d4af9ac84efb2f3aa3
OtaraAlex <mbechealex61@gmail.com>
2023-07-26 17:27:02 +0300

    make changes

--- components/property-card.js
+++ components/property-card.js
@@ -1,54 +1,37 @@
-import { useMemo } from "react";
-
-const PropertyCard = ({
-  imageDimensions,
-  imageDimensionsText,
-  imageDimensionsText2,
-  imageDimensionsText3,
-  imageDimensionsText4,
-  imageDimensionsText5,
-  imageDimensionsText6,
-  imageDimensionsText7,
-  propWidth,
-  propFlex,
-  propMinWidth,
-  propMaxWidth,
-}) => {
-  const card1Style = useMemo(() => {
-    return {
-      width: propWidth,
-      flex: propFlex,
-      minWidth: propMinWidth,
-      maxWidth: propMaxWidth,
-    };
-  }, [propWidth, propFlex, propMinWidth, propMaxWidth]);
-
+const PropertyCard = ({ imgUrl, name, price }) => {
   return (
     <div
       className="rounded-3xs bg-gray-white box-border w-[350px] h-[466.99px] flex flex-col py-[15px] px-[13px] items-start justify-start gap-[23px] text-left text-[14.51px] text-gray-700 font-body-regular-600 border-[1px] border-solid border-whitesmoke-100 hover:bg-gainsboro hover:cursor-pointer sm:w-[100%!important] sm:mb-5"
-      style={card1Style}
     >
       <img
         className="self-stretch relative rounded-3xs max-w-full overflow-hidden h-[200px] shrink-0 object-cover"
         alt=""
-        src={imageDimensions}
+        src={imgUrl}
       />
       <div className="self-stretch flex flex-row p-2.5 items-start justify-start">
         <div className="flex-1 relative leading-[20.32px] font-medium">
-          92 ALLIUM PLACE, ORLANDO FL 32827
+          {name}
         </div>
       </div>
       <div className="self-stretch flex flex-row p-2.5 items-start justify-start text-[13.55px] text-primary-500">
         <div className="relative leading-[18.98px] font-semibold">
-          $ 590,693
+          $ {price}
         </div>
       </div>
-      <div className="self-stretch flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500">
+      <div className="flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500">
         <div className="flex flex-row items-center justify-start gap-[4.35px]">
           <img
-            className="relative w-[17.42px] h-[17.42px]"
+            className="w-[17.42px] h-[17.42px]"
             alt=""
-            src={imageDimensionsText}
+            src="/car.svg"
+          />
+          <div className="relative leading-[16.27px] font-medium">4</div>
+        </div>
+        <div className="flex flex-row items-center justify-start gap-[4.35px]">
+          <img
+            className="w-[17.42px] h-[17.42px]"
+            alt=""
+            src="/bathtub.svg"
           />
           <div className="relative leading-[16.27px] font-medium">4</div>
         </div>
@@ -56,15 +39,7 @@ const PropertyCard = ({
           <img
             className="relative w-[17.42px] h-[17.42px]"
             alt=""
-            src={imageDimensionsText2}
-          />
-          <div className="relative leading-[16.27px] font-medium">4</div>
-        </div>
-        <div className="flex flex-row items-center justify-start gap-[4.35px]">
-          <img
-            className="relative w-[17.42px] h-[17.42px]"
-            alt=""
-            src={imageDimensionsText3}
+            src="/arrowsout.svg"
           />
           <div className="relative leading-[16.27px] font-medium">
             2,096.00 ft
@@ -74,9 +49,9 @@ const PropertyCard = ({
       <div className="self-stretch flex flex-row items-center justify-between text-center text-[11.61px]">
         <div className="flex flex-row items-center justify-start gap-[5.81px]">
           <img
-            className="relative rounded-[50%] w-[27.57px] h-[27.57px] object-cover"
+            className="rounded-[50%] w-[27.57px] h-[27.57px] object-cover"
             alt=""
-            src={imageDimensionsText4}
+            src="/ellipse-1@2x.png"
           />
           <div className="relative leading-[17.42px] font-medium">
             Jenny Wilson
@@ -85,23 +60,23 @@ const PropertyCard = ({
         <div className="flex flex-row items-start justify-start gap-[8.71px]">
           <div className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start">
             <img
-              className="relative w-[14.51px] h-[14.51px]"
+              className="w-[14.51px] h-[14.51px]"
               alt=""
-              src={imageDimensionsText5}
+              src="/sharenetwork.svg"
             />
           </div>
           <div className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start">
             <img
-              className="relative w-[14.51px] h-[14.51px]"
+              className="w-[14.51px] h-[14.51px]"
               alt=""
-              src={imageDimensionsText6}
+              src="/heart.svg"
             />
           </div>
           <div className="rounded-[1.45px] bg-primary-50 flex flex-row p-[2.902620315551758px] items-start justify-start">
             <img
-              className="relative w-[14.51px] h-[14.51px]"
+              className="w-[14.51px] h-[14.51px]"
               alt=""
-              src={imageDimensionsText7}
+              src="/plus.svg"
             />
           </div>
         </div>
